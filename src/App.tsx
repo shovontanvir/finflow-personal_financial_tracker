a2116578013af5166e6805eaec9b3c6b4b68c612
shovontanvir <shovon.tanvir.91@gmail.com>
2026-03-28 14:22:48 +0600

    feat: add tailwindcss and shadcn library to enhance UI components and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <main>
-      <h1>Finflow - your personal financial tracking application</h1>
+      <h1 className="text-5xl">
+        Finflow - your personal financial tracking application
+      </h1>
+      <Button className="mt-4" variant="outline">
+        Get Started
+      </Button>
     </main>
   );
 };
