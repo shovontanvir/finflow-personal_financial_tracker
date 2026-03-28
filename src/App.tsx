7b93ee55b313139bf9c01f152993cbf85caf9122
shovontanvir <shovon.tanvir.91@gmail.com>
2026-03-28 14:29:49 +0600

    feat: integrate react-query for data fetching in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
 import { Button } from "./components/ui/button";
+import { apiMethods } from "./services/api";
 
 const App = () => {
+  const { data } = useQuery({
+    queryKey: ["transactions"],
+    queryFn: async () => apiMethods.get(),
+  });
+
+  console.log(data);
+
   return (
     <main>
       <h1 className="text-5xl">
